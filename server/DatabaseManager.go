ce41e0a7b3619217771a49a211c5a8b83f7fd49f
Luis E Limon <luise_limon@intuit.com>
2018-06-28 15:10:17 -0700

    Changes to support better build system and cobra

---
+++ server/DatabaseManager.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"database/sql"
+)
+
+/*GetDatabaseConnection opens, test and returns a new database connection*/
+func GetDatabaseConnection() *sql.DB {
+	var err error
+	var db *sql.DB
+
+	databaseIP, err := config.GetString("database/host", "localhost")
+	databasePort, err := config.GetString("database/port", "3306")
+	databasePassword, err := config.GetString("database/password", "")
+	databaseType, err := config.GetString("database/type", "mysql")
+	databaseName, err := config.GetString("database/name", "kubeam")
+	databaseUser, err := config.GetString("database/user", "sample-user")
+	databaseConn := databaseUser + databasePassword +
+		"@tcp(" + databaseIP + ":" + databasePort + ")/" + databaseName
+
+	if db, err = sql.Open(databaseType, databaseConn); err == nil {
+		if err = db.Ping(); err == nil {
+			return db
+		}
+	}
+	LogError.Println(err.Error())
+	return nil
+}
